2434885039073d6060c76f049992b85e1236ff8d
devinschumacher <devin@serp.co>
2025-05-09 05:31:41 -0700

    [hotkey]

---
+++ app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  studio: {
+    enabled: true,
+    title: 'Apex Digital CMS',
+    logo: {
+      light: '/favicon.svg',
+      dark: '/favicon.svg'
+    }
+  },
+  ui: {
+    primary: 'blue',
+    gray: 'neutral'
+  }
+})
